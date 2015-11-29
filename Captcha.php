4d5982c281d59e8de0276addf1a0c74541ffca12
tumscorpion <prateep2007@gmail.com>
2015-11-29 10:16:56 +0700

    Structure Code

---
+++ Captcha.php
@@ -0,0 +1,20 @@
+<?php 
+class Captcha
+{
+	public function getQuestion($pattern_number){
+		return "1 + five";
+	}
+
+	public function getOperator(){
+
+	}
+
+	public function getNumber(){
+
+	}
+
+	public function getTextNumber(){
+
+	}
+}
+ ?>
---
+++ CaptchaTest.php
@@ -0,0 +1,6 @@
+<?php 
+class CaptchaTest extends PHPUnit_Framework_TestCase
+{
+
+
+ ?>
