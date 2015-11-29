09b8eec324d8fac47aa13bc235e617f863d1b1d3
tumscorpion <prateep2007@gmail.com>
2015-11-29 12:02:40 +0700

    testResultPatternCaptcha

--- Captcha.php
+++ Captcha.php
@@ -1,20 +1,75 @@
 <?php 
 class Captcha
 {
-	public function getQuestion($pattern_number){
-		return "1 + five";
+	private $pattern;
+	private $right_operand;
+	private $left_operand;
+	private $operator_type;
+	private $result;
+
+	public function getTextNumber($number_key)
+	{
+		$text_number = [
+			0 => 'zero',
+			1 => 'one',
+			2 => 'two',
+			3 => 'three',
+			4 => 'four',
+			5 => 'five',
+			6 => 'six',
+			7 => 'seven',
+			8 => 'eight',
+			9 => 'nine',
+			10 => 'ten',
+		];
+		return $text_number[$number_key];
+
 	}
 
+	public function setPattern($pattern){
+		$this->pattern = $pattern;
+	} 
+	public function setRightOperand($number){
+		$this->right_operand = $number;
+	}
+	public function getRightOperand(){
+		if($this->pattern == 1){
+			$text_number = $this->getTextNumber($this->right_operand);
+			$this->right_operand = $text_number;
+		}
+		// if($this->pattern == 2){
+		// 	$this->right_operand = 4;
+		// }
+		return $this->right_operand;
+	}
+
+	public function setOperator($type){
+		$this->operator_type = $type;
+	}
 	public function getOperator(){
-
+		$Operator = [
+			1=> '+',
+			2=> '-',
+		];
+		return $Operator[$this->operator_type];
+	}
+	public function setLeftOperand($number){
+		$this->left_operand = $number;
+	}
+	public function getLeftOperand(){
+		// if($this->pattern == 1){
+		// 	$this->left_operand = 3;
+		// }
+		if($this->pattern == 2){
+			$text_number = $this->getTextNumber($this->left_operand);
+			$this->left_operand = $text_number;
+		}
+		return $this->left_operand;
 	}
 
-	public function getNumber(){
-
+	public function getResultCaptcha(){
+		return $this->getLeftOperand()." ".$this->getOperator()." ".$this->getRightOperand()." = ?";
 	}
 
-	public function getTextNumber(){
-
-	}
 }
  ?>
--- CaptchaTest.php
+++ CaptchaTest.php
@@ -1,6 +1,84 @@
 <?php 
 class CaptchaTest extends PHPUnit_Framework_TestCase
 {
+	public function testGetTextNumberWith6ReturnSix(){
+		$captcha = new Captcha();
+		$this->assertEquals('zero',$captcha->getTextNumber(0));
+	}
 
+	public function testSetRightOperandPattern1With5ReturnFive()
+	{
+		$captcha = new Captcha();
+		$captcha->setPattern(1);
+		$captcha->setRightOperand(5);
+		$this->assertEquals('five',$captcha->getRightOperand());
+	}
+	public function testSetLeftOperandPattern1With3Return3()
+	{
+		$captcha = new Captcha();
+		$captcha->setPattern(1);
+		$captcha->setLeftOperand(3);
+		$this->assertEquals(3,$captcha->getLeftOperand());
+	}
+	public function testSetRightOperandPattern2With4Return4()
+	{
+		$captcha = new Captcha();
+		$captcha->setPattern(2);
+		$captcha->setRightOperand(4);
+		$this->assertEquals(4,$captcha->getRightOperand());
+	}
+	public function testSetLeftOperandPattern2With7ReturnSeven()
+	{
+		$captcha = new Captcha();
+		$captcha->setPattern(2);
+		$captcha->setLeftOperand(7);
+		$this->assertEquals('seven',$captcha->getLeftOperand());
+	}
 
+	public function testGetOperator1ReturnPlusOperator()
+	{
+		$captcha = new Captcha();
+		$captcha->setOperator(1);
+		$this->assertEquals('+',$captcha->getOperator());
+	}
+	public function testGetOperator2ReturnMinusOperator()
+	{
+		$captcha = new Captcha();
+		$captcha->setOperator(2);
+		$this->assertEquals('-',$captcha->getOperator());
+	}
+	public function testResultPattern1Operator1(){
+		$captcha = new Captcha();
+		$captcha->setPattern(1);
+		$captcha->setOperator(1);
+		$captcha->setLeftOperand(2);
+		$captcha->setRightOperand(1);
+		$this->assertEquals('2 + one = ?',$captcha->getResultCaptcha());
+	}
+	public function testResultPattern2Operator1(){
+		$captcha = new Captcha();
+		$captcha->setPattern(2);
+		$captcha->setOperator(1);
+		$captcha->setLeftOperand(3);
+		$captcha->setRightOperand(2);
+		$this->assertEquals('three + 2 = ?',$captcha->getResultCaptcha());
+	}
+	public function testResultPattern1Operator2(){
+		$captcha = new Captcha();
+		$captcha->setPattern(1);
+		$captcha->setOperator(2);
+		$captcha->setLeftOperand(7);
+		$captcha->setRightOperand(3);
+		$this->assertEquals('7 - three = ?',$captcha->getResultCaptcha());
+	}
+	public function testResultPattern2Operator2(){
+		$captcha = new Captcha();
+		$captcha->setPattern(2);
+		$captcha->setOperator(2);
+		$captcha->setLeftOperand(7);
+		$captcha->setRightOperand(3);
+		$this->assertEquals('seven - 3 = ?',$captcha->getResultCaptcha());
+	}
+
+}
  ?>
